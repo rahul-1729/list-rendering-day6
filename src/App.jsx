37ea72cd16de149611229f8f15731463082d6e6e
rahul-1729 <rk9763981@gmail.com>
2024-06-18 05:51:04 +0530

    basics

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import List from './List.jsx' 
+function App() {
+  
+  // const fruits =
+  //   [{id:1,name : "apple", calories: 95},
+  //    {id:2,name : "orange", calories: 45},
+  //   {id:3,name : "banana", calories: 105},
+  //   {id: 4,name : "coconut", calories:159},
+    // {id: 5,name : "pineapple", calories:37}];
+
+  return( <List/>);
+}
+
+export default App
